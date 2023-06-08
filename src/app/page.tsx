5cbba28bc70f891fb35b6b50672ef6537e8a83fe
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-06-08 13:19:54 +0200

    feat: Create the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <main>
-     <h1>Hello world</h1>
-    </main>
-  )
+	return (
+		<main>
+			<div className='flex justify-between items-center'>
+				<h1 className='text-2xl'>Todo</h1>
+				<Link href='/todo'>
+					<button className='border border-slate-100 hover:bg-blue-700 font-bold py-2 px-4 rounded'>
+						New
+					</button>
+				</Link>
+			</div>
+		</main>
+	);
 }
