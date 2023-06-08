1a2a4b2c0787241461e8498ea0a9f827de51933d
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-06-08 15:16:50 +0200

    Create TodoItem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
 import Link from 'next/link';
+import TodoItem, { TodoProps } from '@/components/TodoItem';
 
 export default function Home() {
+	const todos: TodoProps[] = [
+		{
+			id: '1',
+			title: 'Todo 1',
+			completed: false,
+		},
+		{
+			id: '2',
+			title: 'Todo 2',
+			completed: false,
+		},
+	];
 	return (
 		<main>
 			<div className='flex justify-between items-center'>
@@ -10,6 +23,13 @@ export default function Home() {
 						New
 					</button>
 				</Link>
+			</div>
+			<div>
+				<ul className='ml-4 mt-4 flex flex-col space-y-4'>
+					{todos.map(todo => (
+						<TodoItem key={todo.id} {...todo} />
+					))}
+				</ul>
 			</div>
 		</main>
 	);
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,63 @@
+'use client';
+import { useState } from 'react';
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { CheckIcon, TrashIcon } from '@radix-ui/react-icons';
+
+export interface TodoProps {
+	id: string;
+	title: string;
+	completed: boolean;
+}
+
+export default function TodoItem({ id, title, completed }: TodoProps) {
+	const [isChecked, setIsChecked] = useState(false);
+	const [isHovered, setIsHovered] = useState(false);
+
+	const handleCheckboxChange = (checked: boolean) => {
+		setIsChecked(checked);
+	};
+
+	const handleMouseEnter = () => {
+		setIsHovered(true);
+	};
+
+	const handleMouseLeave = () => {
+		setIsHovered(false);
+	};
+
+	return (
+		// <div>
+		<div className='flex items-center'>
+			<Checkbox.Root
+				className='appearance-none bg-slate-100 h-6 w-6 border rounded flex items-center justify-center hover:bg-slate-300'
+				id={id}
+				checked={isChecked}
+				onCheckedChange={handleCheckboxChange}
+			>
+				<Checkbox.Indicator className='text-slate-500'>
+					<CheckIcon />
+				</Checkbox.Indicator>
+			</Checkbox.Root>
+			<label
+				className={`pl-4 leading-none ${
+					isChecked ? 'line-through text-slate-200' : ''
+				}`}
+				htmlFor={id}
+			>
+				{title}
+			</label>
+		</div>
+		// {/* <div className="flex justify-around">
+		//   <TrashIcon
+		//     className="appearance-none h-6 w-6 hover:text-blue-500 cursor-pointer"
+		//     onMouseEnter={handleMouseEnter}
+		//     onMouseLeave={handleMouseLeave}
+		//     onClick={() => {
+		//       console.log('clicked');
+		//     }}
+		//   />
+		//   {isHovered && <div className="text-red-500">Delete?</div>}
+		// </div> */}
+		// </div>
+	);
+}
