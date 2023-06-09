15d41b2ba1d2acbb592b72f2a714da638af8efcf
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-06-09 15:07:59 +0200

    feat: Add trash icon to todo

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -14,7 +14,6 @@ export interface TodoProps {
 export default function TodoItem({ id, title, completed }: TodoProps) {
 	const [updateTodoCompleted] = useMutation(UPDATE_TODO);
 	const [isChecked, setIsChecked] = useState(completed);
-	const [isHovered, setIsHovered] = useState(false);
 
 	const handleCheckboxChange = (checked: boolean) => {
 		updateTodoCompleted({
@@ -23,17 +22,8 @@ export default function TodoItem({ id, title, completed }: TodoProps) {
 		setIsChecked(checked);
 	};
 
-	const handleMouseEnter = () => {
-		setIsHovered(true);
-	};
-
-	const handleMouseLeave = () => {
-		setIsHovered(false);
-	};
-
 	return (
-		// <div>
-		<div className='flex items-center'>
+		<div className='flex items-center w-80'>
 			<Checkbox.Root
 				className='appearance-none bg-slate-100 h-6 w-6 border rounded flex items-center justify-center hover:bg-slate-300'
 				id={id}
@@ -45,25 +35,19 @@ export default function TodoItem({ id, title, completed }: TodoProps) {
 				</Checkbox.Indicator>
 			</Checkbox.Root>
 			<label
-				className={`pl-4 leading-none ${
+				className={`pl-4 leading-none truncate ${
 					isChecked ? 'line-through text-slate-200' : ''
 				}`}
 				htmlFor={id}
 			>
 				{title}
 			</label>
+			<TrashIcon
+				className='appearance-none h-6 w-6 text-red-500 cursor-pointer ml-auto'
+				onClick={() => {
+					console.log('clicked');
+				}}
+			/>
 		</div>
-		// {/* <div className="flex justify-around">
-		//   <TrashIcon
-		//     className="appearance-none h-6 w-6 hover:text-blue-500 cursor-pointer"
-		//     onMouseEnter={handleMouseEnter}
-		//     onMouseLeave={handleMouseLeave}
-		//     onClick={() => {
-		//       console.log('clicked');
-		//     }}
-		//   />
-		//   {isHovered && <div className="text-red-500">Delete?</div>}
-		// </div> */}
-		// </div>
 	);
 }
